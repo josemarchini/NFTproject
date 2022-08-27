518afbe6d4777016f1366d632f7c643e3e4658d5
Jose Marchini <jmarchinisarmiento@gmail.com>
2022-08-27 17:41:28 +0200

    cambios

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,36 +1,17 @@
 
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const projectId = process.env.INFURA_PROJECT_ID;
+const privateKey = process.env.DEPLOYER_SIGNER_PRIVATE_KEY;
 
 module.exports = {
-  defaultNetwork: "ropsten",
+  solidity: "0.8.4",
   networks: {
-    hardhat: {
-    },
     ropsten: {
-      url: "https://ropsten.infura.io/v3/b6528fcccdf441639fc77a8f69bc3c2e",
-      accounts: [572c3ca09ab4693845c29c2cdfbf738ed30619d5030eb50f1e3ed9500c273f86]
-    }
+      url: `https://ropsten.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
+    },
   },
-  solidity: {
-    version: "0.8.9",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
-    }
-  },
-  paths: {
-    sources: "./contracts",
-    tests: "./test",
-    cache: "./cache",
-    artifacts: "./artifacts"
-  },
-  mocha: {
-    timeout: 40000
-  }
-}
-
+};
 
