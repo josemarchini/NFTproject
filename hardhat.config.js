617c99c877a7770c28ca46e38c0d2f795761a66e
Jose Marchini <jmarchinisarmiento@gmail.com>
2022-08-27 17:03:51 +0200

    se isieron algunos cambios al network

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,12 +2,35 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
-  solidity: "0.8.9",
+  defaultNetwork: "ropsten",
   networks: {
-    ropsten: {
-      url: "https://ropsten.infura.io/v3/b6528fcccdf441639fc77a8f69bc3c2",
-      accounts: ["0x572c3ca09ab4693845c29c2cdfbf738ed30619d5030eb50f1e3ed9500c273f86"],
+    hardhat: {
     },
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/b6528fcccdf441639fc77a8f69bc3c2e",
+      accounts: [572c3ca09ab4693845c29c2cdfbf738ed30619d5030eb50f1e3ed9500c273f86]
+    }
   },
-};
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000
+  }
+}
+
+
